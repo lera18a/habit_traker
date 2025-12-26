cc36689cbf6ea450e7afd5dcda8160634904176f
Badrakova Valeria <lera_18a@mail.ru>
2025-12-26 12:33:38 +0300

    fix(repository): fix delete habit logic

--- lib/data_layer/repository/habit_repository.dart
+++ lib/data_layer/repository/habit_repository.dart
@@ -32,32 +32,27 @@ class HabitRepository with HabitRepositoryMixin {
   }
 
   @override
-  Stream<List<Habit>> get readHabit async* {
-    while (true) {
-      final habitRows = await db.query('habits');
-      final List<Habit> habits = [];
+  Future<List<Habit>> get readHabit async {
+    final habitRows = await db.query('habits');
+    final List<Habit> habits = [];
 
-      for (final h in habitRows) {
-        final habitId = h['id'] as int;
+    for (final h in habitRows) {
+      final habitId = h['id'] as int;
 
-        final weekRows = await db.query(
-          'week',
-          where: 'habit_id = ?',
-          whereArgs: [habitId],
-          limit: 1,
-        );
+      final weekRows = await db.query(
+        'week',
+        where: 'habit_id = ?',
+        whereArgs: [habitId],
+        limit: 1,
+      );
 
-        if (weekRows.isEmpty) continue;
+      if (weekRows.isEmpty) continue;
 
-        final weekMap = weekRows.first;
+      final weekMap = weekRows.first;
 
-        habits.add(Habit.fromHabitAndWeekMaps(h, weekMap));
-      }
-
-      yield habits;
-
-      await Future.delayed(const Duration(seconds: 1));
+      habits.add(Habit.fromHabitAndWeekMaps(h, weekMap));
     }
+    return habits;
   }
 
   @override
--- lib/data_layer/repository/habit_repository_mixin.dart
+++ lib/data_layer/repository/habit_repository_mixin.dart
@@ -4,7 +4,7 @@ import 'package:habit_traker/data_layer/models/habit.dart';
 
 mixin HabitRepositoryMixin {
   Future<void> createHabit(Habit habit);
-  Stream<List<Habit>> get readHabit;
+  Future<List<Habit>> get readHabit;
   Future<void> updateHabit(Habit habit);
   Future<void> deleteHabit(Habit habit);
 }
